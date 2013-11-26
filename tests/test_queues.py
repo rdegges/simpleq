905739f5910357076adda2e6f61d0cb4b99af67f
Randall Degges <r@rdegges.com>
2013-11-25 23:11:41 -0800

    Adding some tests for the Queue.

---
+++ tests/test_queues.py
@@ -0,0 +1,20 @@
+from uuid import uuid4
+
+from boto.sqs import connect_to_region
+from boto.sqs.queue import Queue as SQSQueue
+from sqsq.queues import Queue
+
+
+class TestQueue:
+
+    def test_lazy_create_queue(self):
+        sid = uuid4().hex
+        Queue(sid)
+
+        assert not connect_to_region('us-east-1').get_queue(sid)
+
+    def test_create_queue(self):
+        sid = uuid4().hex
+        q = Queue(sid)
+
+        assert isinstance(q.queue, SQSQueue)
