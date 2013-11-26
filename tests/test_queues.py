6486cad2c24763ec3f08fde20545207975748ca6
Randall Degges <r@rdegges.com>
2013-11-25 23:28:34 -0800

    Adding a test for our Queue.enqueue method.

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,8 +1,14 @@
+from time import sleep
 from uuid import uuid4
 
 from boto.sqs import connect_to_region
 from boto.sqs.queue import Queue as SQSQueue
 from sqsq.queues import Queue
+
+
+def test_job(arg1=None, arg2=None):
+    """This is a test job."""
+    pass
 
 
 class TestQueue:
@@ -18,4 +24,15 @@ class TestQueue:
         q = Queue(sid)
 
         assert isinstance(q.queue, SQSQueue)
+        q._connection.delete_queue(q.queue)
+
+    def test_enqueue(self):
+        sid = uuid4().hex
+        q = Queue(sid)
+
+        q.enqueue(test_job, 'there')
+        q.enqueue(test_job, arg1='test', arg2='test')
+        sleep(10)
+
+        assert len(q.messages) == 2
         q._connection.delete_queue(q.queue)
