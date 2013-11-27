d93b4128eb1f5a276c34035c3627f99d60dcd4f9
Randall Degges <r@rdegges.com>
2013-11-26 18:27:26 -0800

    Adding tests for our Job class.

---
+++ tests/test_jobs.py
@@ -0,0 +1,32 @@
+from time import sleep
+
+from sqsq.jobs import Job
+
+
+def random_job(arg1=None, arg2=None):
+    """A sample job for testing."""
+    sleep(1)
+    return 'yo!'
+
+
+def bad_job():
+    return 1 / 0
+
+
+class TestJob:
+
+    def test_create_job(self):
+        job = Job(random_job, 'hi', arg2='there')
+        assert isinstance(job, Job)
+
+    def test_run(self):
+        job = Job(random_job, 'hi', arg2='there')
+        job.run()
+
+        assert job.run_time >= 1
+        assert job.result == 'yo!'
+
+        job = Job(bad_job)
+        job.run()
+
+        assert isinstance(job.exception, ZeroDivisionError)
