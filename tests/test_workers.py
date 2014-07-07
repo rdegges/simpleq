1509106da034d395feb38bf7f56f92dd30227ab8
Randall Degges <r@rdegges.com>
2014-07-06 17:20:02 -0700

    Adding in worker tests.

---
+++ tests/test_workers.py
@@ -0,0 +1,41 @@
+"""All of our worker tests."""
+
+
+from time import sleep
+from unittest import TestCase
+from uuid import uuid4
+
+from simpleq.jobs import Job
+from simpleq.queues import Queue
+from simpleq.workers import Worker
+
+
+def test_job(arg1=None, arg2=None):
+    """This is a test job."""
+    pass
+
+
+class TestWorker(TestCase):
+
+    def test_initialize(self):
+        sid = uuid4().hex
+        queue = Queue(sid)
+        worker = Worker([queue])
+
+        self.assertIsInstance(worker, Worker)
+
+    def test_work(self):
+        sid = uuid4().hex
+        queue = Queue(sid)
+        queue.add_job(Job(test_job, 'hi', 'there'))
+
+        sleep(10)
+        self.assertEqual(len(queue.jobs), 1)
+
+        worker = Worker([queue])
+        worker.work(True)
+
+        sleep(10)
+
+        self.assertEqual(len(queue.jobs), 0)
+        queue.delete()
